725556ebf7d32c944167ed89aa9e397dd70a7f72
gingjin <gingjin@163.com>
2022-10-03 10:10:33 +0800

    update info of st

--- config.def.h
+++ config.def.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
+// static char *font = "JetBrainsMono Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Monaco:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
